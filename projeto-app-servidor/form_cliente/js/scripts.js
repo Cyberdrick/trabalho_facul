e57ee272a9c37142bbf37e84fbff8fcfd2627306
Rodrigo Sanchez <rodrigoschort01@gmail.com>
2023-11-08 16:03:43 -0300

    ultima das ultimas

--- projeto-app-servidor/form_cliente/js/scripts.js
+++ projeto-app-servidor/form_cliente/js/scripts.js
@@ -1,208 +1,56 @@
-class Validator {
-
-  constructor() {
-    this.validations = [
-      'data-min-length',
-      'data-max-length',
-      'data-only-letters',
-      'data-email-validate',
-      'data-required',
-      'data-equal',
-      'data-password-validate',
-    ]
-  }
-
-  // inicia a validação de todos os campos
-  validate(form) {
-
-    // limpa todas as validações antigas
-    let currentValidations = document.querySelectorAll('form .error-validation');
-
-    if(currentValidations.length) {
-      this.cleanValidations(currentValidations);
-    }
-
-    // pegar todos inputs
-    let inputs = form.getElementsByTagName('input');
-    // transformar HTMLCollection em arr
-    let inputsArray = [...inputs];
-
-    // loop nos inputs e validação mediante aos atributos encontrados
-    inputsArray.forEach(function(input, obj) {
-
-      // fazer validação de acordo com o atributo do input
-      for(let i = 0; this.validations.length > i; i++) {
-        if(input.getAttribute(this.validations[i]) != null) {
-
-          // limpa string para saber o método
-          let method = this.validations[i].replace("data-", "").replace("-", "");
-
-          // valor do input
-          let value = input.getAttribute(this.validations[i])
-
-          // invoca o método
-          this[method](input,value);
-
-        }
-      }
-
-    }, this);
-
-  }
-
-  // método para validar se tem um mínimo de caracteres
-  minlength(input, minValue) {
-
-    let inputLength = input.value.length;
-
-    let errorMessage = `O campo precisa ter pelo menos ${minValue} caracteres`;
-
-    if(inputLength < minValue) {
-      this.printMessage(input, errorMessage);
-    }
-
-  }
-
-  // método para validar se passou do máximo de caracteres
-  maxlength(input, maxValue) {
-
-    let inputLength = input.value.length;
-
-    let errorMessage = `O campo precisa ter menos que ${maxValue} caracteres`;
-
-    if(inputLength > maxValue) {
-      this.printMessage(input, errorMessage);
-    }
-
-  }
-
-  // método para validar strings que só contem letras
-  onlyletters(input) {
-
-    let re = /^[0-9]+$/;;
-
-    let inputValue = input.value;
-
-    let errorMessage = `Este campo aceita somente números.`;
-
-    if(!re.test(inputValue)) {
-      this.printMessage(input, errorMessage);
-    }
-
-  }
-
-  // método para validar e-mail
-  emailvalidate(input) {
-    let re = /\S+@\S+\.\S+/;
-
-    let email = input.value;
-
-    let errorMessage = `Insira um e-mail no padrão matheus@email.com`;
-
-    if(!re.test(email)) {
-      this.printMessage(input, errorMessage);
-    }
-
-  }
-
-  // verificar se um campo está igual o outro
-  equal(input, inputName) {
-
-    let inputToCompare = document.getElementsByName(inputName)[0];
-
-    let errorMessage = `Este campo precisa estar igual ao ${inputName}`;
-
-    if(input.value != inputToCompare.value) {
-      this.printMessage(input, errorMessage);
-    }
-  }
-  
-  // método para exibir inputs que são necessários
-  required(input) {
-
-    let inputValue = input.value;
-
-    if(inputValue === '') {
-      let errorMessage = `Este campo é obrigatório`;
-
-      this.printMessage(input, errorMessage);
-    }
-
-  }
-
-  // validando o campo de senha
-  passwordvalidate(input) {
-
-    // explodir string em array
-    let charArr = input.value.split("");
-
-    let uppercases = 0;
-    let numbers = 0;
-
-    for(let i = 0; charArr.length > i; i++) {
-      if(charArr[i] === charArr[i].toUpperCase() && isNaN(parseInt(charArr[i]))) {
-        uppercases++;
-      } else if(!isNaN(parseInt(charArr[i]))) {
-        numbers++;
-      }
-    }
-
-    if(uppercases === 0 || numbers === 0) {
-      let errorMessage = `A senha precisa um caractere maiúsculo e um número`;
-
-      this.printMessage(input, errorMessage);
-    }
-
-  }
-
-  // método para imprimir mensagens de erro
-  printMessage(input, msg) {
-  
-    // checa os erros presentes no input
-    let errorsQty = input.parentNode.querySelector('.error-validation');
-
-    // imprimir erro só se não tiver erros
-    if(errorsQty === null) {
-      let template = document.querySelector('.error-validation').cloneNode(true);
-
-      template.textContent = msg;
-  
-      let inputParent = input.parentNode;
-  
-      template.classList.remove('template');
-  
-      inputParent.appendChild(template);
-    }
-
-  }
-
-  // remove todas as validações para fazer a checagem novamente
-  cleanValidations(validations) {
-    validations.forEach(el => el.remove());
-  }
-
-}
-
 let validator = new Validator();
 
-const submit = document.getElementById('btn-submit');
-
-// Função para salvar dados no localStorage
-
-function saveDataToLocalStorage() {
+// Função de validação
+function validateForm() {
   const email = document.querySelector("#email").value;
   const name = document.querySelector("#name").value;
   const cpf = document.querySelector("#cpf").value;
   const password = document.querySelector("#password").value;
+  const passwordconfirmation = document.querySelector("#passwordconfirmation").value;
 
+  // Adicione aqui as condições de validação desejadas
+  if (email === "" || name === "" || cpf === "" || password === "" || passwordconfirmation === "") {
+    alert("Preencha todos os campos do formulário.");
+    return false; // Impede o envio do formulário
+  }
 
-  // Criar um objeto com os Dados
+  if (password !== passwordconfirmation) {
+    alert("As senhas não coincidem.")
+    return false;
+  }
 
-  const userData = {
-    email: email,
-    name: name,
-    cpf: cpf,
-    password: password,
+  // Outras condições de validação, se necessário
+  // Exemplo: Verificar se o e-mail tem um formato válido
+
+  return true; // Permite o envio do formulário se a validação for bem-sucedida
+}
+
+// Função para salvar dados no localStorage
+
+function saveDataToLocalStorage() {
+  const isValid = validateForm();
+
+    if (isValid) {
+    const email = document.querySelector("#email").value;
+    const name = document.querySelector("#name").value;
+    const cpf = document.querySelector("#cpf").value;
+    const password = document.querySelector("#password").value;
+    const passwordconfirmation = document.querySelector("#passwordconfirmation").value;
+
+    // Criar um objeto com os Dados
+    const userData = {
+      email: email,
+      name: name,
+      cpf: cpf,
+      password: password,
+      passwordconfirmation: passwordconfirmation,
+    };
+
+    // Converter o objeto em string JSON
+    const userDataJSON = JSON.stringify(userData);
+
+    // Armazenar a string JSON no localStorage
+    localStorage.setItem("userData", userDataJSON);
   };
 
   // Converter o objeto em string JSON
